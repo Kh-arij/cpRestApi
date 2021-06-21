dfc396e92c7d18bdd024ae042cb08f998b14861b
Arij <khaldi.arij9@gmail.com>
2021-06-21 14:16:09 +0100

    this cp  for rest api

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import UserList from "./components/UserList";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+const Navbar = () => {
+  return (
+    <header className="navbar">
+      <h1>
+        <i className="fas fa-user" /> Contact App
+      </h1>
+    </header>
+  );
+};
+
+export default Navbar;
